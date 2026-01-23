6cc06bee98aa7c5beb82612ce62e75d018f85a47
Suikyoo <judeanthony02sayson@gmail.com>
2026-01-23 09:28:50 +0800

    fixed gravity direction by angle

--- scripts/world.gd
+++ scripts/world.gd
@@ -17,8 +17,7 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("change_perspective"):
 		target_angle += rot_scalar
-		world_gravity = Vector2.from_angle(target_angle - PI/2) * gravity_value
-		
+		world_gravity = Vector2.from_angle(target_angle - PI/2) * Vector2(1, -1) * gravity_value
 		world_flip.emit(target_angle, world_gravity)
 		
 	world_angle = lerp_angle(world_angle, target_angle, 0.4)
