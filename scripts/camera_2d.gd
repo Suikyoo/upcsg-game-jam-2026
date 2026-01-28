c19689cf6851a2f7c38865828469e2180d197cad
pewpil <pewpil@proton.me>
2026-01-28 10:55:28 +0800

    zooooom

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+const zoom_factor: float = 0.1
+
 func _process(delta: float) -> void:
 	
 	var player: Player = get_node("../Player")
@@ -9,3 +11,11 @@ func _process(delta: float) -> void:
 	global_position = player.get_draw_pos()
 	
 	#rotation = 0
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			self.zoom += Vector2(zoom_factor, zoom_factor)
+			
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			self.zoom -= Vector2(zoom_factor, zoom_factor)
