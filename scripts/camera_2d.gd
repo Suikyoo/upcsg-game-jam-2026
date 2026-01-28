cb85c4291f4be627fea9f56251a04381fe93d5b0
pewpil <pewpil@proton.me>
2026-01-28 11:10:39 +0800

    ugly zoom

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 const zoom_factor: float = 0.1
+const zoom_vec: Vector2 = Vector2(zoom_factor, zoom_factor)
 
 func _process(delta: float) -> void:
 	
@@ -12,10 +13,15 @@ func _process(delta: float) -> void:
 	
 	#rotation = 0
 	
+
 func _unhandled_input(event: InputEvent) -> void:
+	const lerp_weight: float = 0.1
+	
+	# Zooming is ugly but it works
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			self.zoom += Vector2(zoom_factor, zoom_factor)
+			self.zoom = lerp(self.zoom, self.zoom +self.zoom_vec, lerp_weight)
+			
 			
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			self.zoom -= Vector2(zoom_factor, zoom_factor)
+			self.zoom = lerp(self.zoom, self.zoom -self.zoom_vec, lerp_weight)
